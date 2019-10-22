1a355b028384836eb81f1dfc37a4c7a4d0a17a39
derekhnguyen <derekhnguyen@ucsb.edu>
2019-10-22 14:00:32 -0700

    added disease mortality and script

---
+++ my_part_3_script.R
@@ -0,0 +1,31 @@
+# Derek Nguyen
+
+# ---------------
+library(tidyverse)
+library(here)
+library(janitor)
+
+# ---------------
+
+db <- read_csv(here::here("data","disease_burden.csv")) %>% 
+  clean_names() %>% 
+  rename(deaths_per_100k = death_rate_per_100_000)
+
+view(db)
+
+# ----------
+
+db_sub <- db %>% 
+  filter(country_name %in% c("United States", "Japan", "Afghanistan", "Somalia")) %>% 
+  filter(age_group == "0-6 days", sex == "Both") 
+
+# ---------
+
+ggplot(db_sub, aes(x = year, y = deaths_per_100k)) +
+  geom_line(aes(color = country_name))
+
+# ------------
+
+ggsave(here::here("final_graphs", "disease_graph.png"))
+
+
